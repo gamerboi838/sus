a3d33c8dd9ffa15bed5fc13b53731e4a4aebd975
gamerboi838 <116967440+gamerboi838@users.noreply.github.com>
2022-11-04 11:13:16 +0530

    Update EXP6.cpp

--- EXP6.cpp
+++ EXP6.cpp
@@ -1,61 +1,46 @@
-
-// Apply Mixed Language Programing to design a calculator
-
-
-// 1.   Code for Left Shift and Right Shift in C++
-
-
 #include <iostream.h>
 #include <conio.h>
-
-int main()
-{
-    clrscr();
-    int a, b, c, d;
-    cout << "Enter Your Number : -  " << endl;
-    cin >> a;
-
-    cout << "Enter No to shift: - " << endl;
-    cin >> b;
-
-    asm mov ax, a;
-    asm mov cx, b;
-    asm shl ax, cl;
-    asm mov c, ax;
-    cout << "Shift left :- " << c << endl;
-
-    cout << "Enter no to shift: - " << endl;
-    cin >> d;
-    asm mov ax, a;
-    asm mov cx, d;
-    asm shr ax, cl;
-    asm mov c, ax;
-
-    cout << "Shift Right :- " << c << endl;
-    getch();
-    return 0;
+void main(){
+clrscr();
+int a,b,result;
+int ch;
+cout<<“Enter two numbers:”;
+cin>>a>>b;
+cout<<“1.Add /n 2.Sub /n 3.Mul /n 4.Div”<<endl;
+cin>>ch;
+switch(ch){
+case 1:{
+asm mov ax,a;
+asm mov bx,b;
+asm add ax,bx;
+asm mov result,ax;
+cout<<“Result:”<<result;
+break;
 }
-
-
-
-
-// 2.  Code for Calculator in .C
-#include<stdio.h>
-#include<conio.h>
-void main() {
-
-   int a = 6, b = 3, c;
-   clrscr();
-   asm {
-      mov ax,a
-      mov bx,b
-      add ax,bx
-      mov c,ax
-   }
-
-   printf("%d",c);
-   getch();
+case 2:{
+asm mov ax,a;
+asm mov bx,b;
+asm sub ax,bx;
+asm mov result,ax;
+cout<<“Result:”<<result;
+break;
 }
-
-
-
+case 3:{
+asm mov ax,a;
+asm mov bx,b;
+asm mul bx;
+asm mov result,ax;
+cout<<“Result:”<<result;
+break;
+}
+case 4:{
+asm mov ax,a;
+asm mov bx,b;
+asm div bx;
+asm mov result,ax;
+cout<<“Result:”<<result;
+break;
+}
+}
+getch();
+}
